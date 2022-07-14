27822ec7e0c876fdf2ac231eabcfb1c374c88221
junho100 <bemodesty306@naver.com>
2022-07-14 23:06:32 +0900

    Feat: 간단 채팅 앱 구현 완료(프론트)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import { Board } from "./board";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
-function App() {
+const socket = io.connect("http://localhost:8080");
+
+const App = () => {
+  const [chat, setChat] = useState("");
+  const [chatList, setChatList] = useState([]);
+
+  useEffect(() => {
+    socket.on("new_chat", (message) => {
+      setChatList([...chatList, message]);
+    });
+  });
+
   return (
-    <div className="App">
-      <Board></Board>
+    <div>
+      <div>
+        {chatList.map((c, index) => {
+          return <div key={index}>{c}</div>;
+        })}
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const message = e.target[0].value;
+          socket.emit("chat", message);
+          setChatList([...chatList, message]);
+          setChat("");
+        }}
+      >
+        <input
+          value={chat}
+          onChange={(e) => {
+            setChat(e.target.value);
+          }}
+        ></input>
+        <button>submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
